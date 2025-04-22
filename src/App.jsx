d7f0706f1e73ebc6b5f92730ffd90e3d054cac29
bbarankaraa <bbarankaraa@gmail.com>
2025-04-22 16:43:48 +0300

    first committtt

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
+import { Route, Routes } from 'react-router-dom'
 import './App.css'
-
-
+import HomePage from './pages/HomePage/HomePage'
 
 
 function App() {
 
   return (
-    <div>
-
-    </div>
+    <Routes>
+      <Route path='/' element={<HomePage />} />
+    </Routes>
   )
 }
 
